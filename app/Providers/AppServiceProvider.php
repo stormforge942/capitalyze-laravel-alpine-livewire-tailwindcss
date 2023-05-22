a821fedafa6a1e086d879dd8ce800108d25b2ef0
ace95322 <ace95322@gmail.com>
2023-05-22 08:33:39 -0300

    remove old file, log sql queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         DB::listen(function($query) {
-            Log::info(
+            Log::error(
                 $query->sql,
                 $query->bindings,
                 $query->time
