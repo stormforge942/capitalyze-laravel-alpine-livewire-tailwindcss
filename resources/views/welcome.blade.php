efdcf7335b2180ca86c5def16154e44def675664
ace95322 <ace95322@gmail.com>
2023-05-26 17:44:03 +0200

    change to asset link for svg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="mx-auto max-w-full max-w-7xl">
             <div class="px-24 py-8 overflow-hidden bg-white shadow-xl sm:rounded-lg ">
-            <object data="{{ URL::to('/') }}/svg/xbrl-process.svg" type="image/svg+xml" alt="xbrl process"></object>
+            <object data="{{ asset('svg/xbrl-process.svg') }}" type="image/svg+xml" alt="xbrl process"></object>
             </div>
         </div>
     </div>
