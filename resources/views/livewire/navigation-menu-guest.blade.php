14bdd4a15c071003350c1f260d3632072ecccca6
ace95322 <ace95322@gmail.com>
2023-05-28 14:42:39 -0300

    add responsive search bar

--- resources/views/livewire/navigation-menu-guest.blade.php
+++ resources/views/livewire/navigation-menu-guest.blade.php
@@ -76,14 +76,6 @@
             <!-- Hamburger -->
 
             <div class="-mr-2 flex items-center sm:hidden">
-                <button wire:click="$emit('spotlight.toggle')" class="mr-5 sm:hidden">
-                    <svg class="h-5 w-5 inline-flex text-md font-medium leading-5 text-gray-500 focus:outline-none focus:border-indigo-700 transition" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
-                        <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                    </svg>
-                    <span class="text-sm font-medium leading-5 text-gray-500">
-                        Search
-                        </span>
-                </button>
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -92,6 +84,18 @@
                 </button>
             </div>
         </div>
+        <div class="sm:hidden flex justify-center justify-items-stretch m-auto flex-grow h-12">
+                <button wire:click="$emit('spotlight.toggle')" class="h-8 w-full grid grid-cols-5 mx-auto max-w-md">
+                    <span class="border-2 border-gray-200 border-r-0 text-sm font-medium leading-5 text-gray-500 justify-self-stretch col-span-4 text-left h-8 px-2 py-1">
+                    {{ __('Search Stocks, Tickers ...') }}
+                    </span>
+                    <div class="bg-blue-500 h-8 px-2 py-1 text-center flex flex-row">
+                        <svg class="h-5 w-5 text-md font-medium leading-5 text-white m-auto" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                </button>
+            </div>
     </div>
 
     <!-- Responsive Navigation Menu -->
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,7 +2,7 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+            <div class="flex justify-self-stretch">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
@@ -18,15 +18,20 @@
                     <x-jet-nav-link href="{{ route('earnings-calendar') }}" :active="request()->routeIs('earnings-calendar')">
                         {{ __('Earnings Calendar') }}
                     </x-jet-nav-link>
-                    <button wire:click="$emit('spotlight.toggle')" class="hover:text-gray-700 hover:border-gray-300 border-b-2 border-transparent focus:outline-none focus:border-indigo-700">
-                        <svg class="h-5 w-5 inline-flex text-md font-medium leading-5 text-gray-500 focus:outline-none focus:border-indigo-700 transition" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                </div>
+            </div>
+
+            <div class="hidden sm:flex justify-center justify-items-stretch m-auto flex-grow px-10">
+                <button wire:click="$emit('spotlight.toggle')" class="h-8 w-full grid grid-cols-5 mx-auto max-w-md">
+                    <span class="border-2 border-gray-200 border-r-0 text-sm font-medium leading-5 text-gray-500 justify-self-stretch col-span-4 text-left h-8 px-2 py-1">
+                    {{ __('Search Stocks, Tickers ...') }}
+                    </span>
+                    <div class="bg-blue-500 h-8 px-2 py-1 text-center flex flex-row">
+                        <svg class="h-5 w-5 text-md font-medium leading-5 text-white m-auto" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
                             <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                         </svg>
-                        <span class="text-sm font-medium leading-5 text-gray-500">
-                        Search
-                        </span>
-                    </button>
-                </div>
+                    </div>
+                </button>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -88,14 +93,6 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button wire:click="$emit('spotlight.toggle')" class="mr-5 sm:hidden">
-                    <svg class="h-5 w-5 inline-flex text-md font-medium leading-5 text-gray-500 focus:outline-none focus:border-indigo-700 transition" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
-                        <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                    </svg>
-                    <span class="text-sm font-medium leading-5 text-gray-500">
-                        Search
-                        </span>
-                </button>
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -104,6 +101,18 @@
                 </button>
             </div>
         </div>
+        <div class="sm:hidden flex justify-center justify-items-stretch m-auto flex-grow h-12">
+                <button wire:click="$emit('spotlight.toggle')" class="h-8 w-full grid grid-cols-5 mx-auto max-w-md">
+                    <span class="border-2 border-gray-200 border-r-0 text-sm font-medium leading-5 text-gray-500 justify-self-stretch col-span-4 text-left h-8 px-2 py-1">
+                    {{ __('Search Stocks, Tickers ...') }}
+                    </span>
+                    <div class="bg-blue-500 h-8 px-2 py-1 text-center flex flex-row">
+                        <svg class="h-5 w-5 text-md font-medium leading-5 text-white m-auto" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                </button>
+            </div>
     </div>
 
     <!-- Responsive Navigation Menu -->
