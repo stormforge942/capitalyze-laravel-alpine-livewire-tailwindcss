ad9b826a72c77bc9d04512067b4741e0adf3bb18
ace95322 <ace95322@gmail.com>
2023-06-14 22:04:49 -0300

    add quarter to view to fund summary

--- app/Http/Livewire/FundSummary.php
+++ app/Http/Livewire/FundSummary.php
@@ -2,29 +2,36 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 
 class FundSummary extends Component
 {
-    public array $topBuys;
-    public array $topSells;
-    public array $summary;
-    public array $activity;
-    public $latestQuarter;
     public $cik;
     public $fund;
+    public $quarters;
+    public $selectedQuarter;
+    public $topBuys;
+    public $topSells;
+    public $summary;
+    public $activity;
 
-    public function mount($cik, $fund) {
+    public function mount($cik, $fund)
+    {
         $this->cik = $cik;
         $this->fund = $fund;
-        $this->latestQuarter = DB::connection('pgsql-xbrl')->table('filings_summary')->max(DB::raw("TO_DATE(date, 'YYYY-MM-DD')"));
+        $this->quarters = $this->generateQuarters();
+        $this->selectedQuarter = array_key_first($this->quarters);
+        $this->loadFundData($this->selectedQuarter);
+    }
 
+    public function loadFundData($quarter) {
         $data = DB::connection('pgsql-xbrl')
         ->table('filings')
         ->select('change_in_shares', 'change_in_value', 'symbol', 'name_of_issuer')
-        ->where('cik', '=', $cik)
-        ->where('report_calendar_or_quarter', '=', $this->latestQuarter)
+        ->where('cik', '=', $this->cik)
+        ->where('report_calendar_or_quarter', '=', $quarter)
         ->orderByDesc('change_in_shares')
         ->limit(5)
         ->get()->toArray();
@@ -35,8 +42,8 @@ class FundSummary extends Component
         $data = DB::connection('pgsql-xbrl')
         ->table('filings')
         ->select('change_in_shares', 'change_in_value', 'symbol', 'name_of_issuer')
-        ->where('cik', '=', $cik)
-        ->where('report_calendar_or_quarter', '=', $this->latestQuarter)
+        ->where('cik', '=', $this->cik)
+        ->where('report_calendar_or_quarter', '=', $quarter)
         ->orderBy('change_in_shares')
         ->limit(5)
         ->get()->toArray();
@@ -46,8 +53,8 @@ class FundSummary extends Component
         $data = DB::connection('pgsql-xbrl')
         ->table('filings')
         ->select('weight', 'symbol', 'name_of_issuer')
-        ->where('cik', '=', $cik)
-        ->where('report_calendar_or_quarter', '=', $this->latestQuarter)
+        ->where('cik', '=', $this->cik)
+        ->where('report_calendar_or_quarter', '=', $quarter)
         ->orderByDesc('weight')
         ->limit(4)
         ->get()->toArray();
@@ -58,13 +65,82 @@ class FundSummary extends Component
         $data = DB::connection('pgsql-xbrl')
         ->table('filings_summary')
         ->select('cik','investor_name','portfolio_size','added_securities','removed_securities','total_value','last_value','change_in_total_value','change_in_total_value_percentage','turnover','turnover_alt_sell','turnover_alt_buy','average_holding_period','average_holding_period_top10','average_holding_period_top20','url')
-        ->where('cik', '=', $cik)
-        ->where('date', '=', $this->latestQuarter)
+        ->where('cik', '=', $this->cik)
+        ->where('date', '=', $quarter)
         ->limit(1)
         ->get();
     
         $this->summary = $data->isEmpty() ? [] : (array) $data->first();
 
+        if (!empty($this->summary)) {
+            $percentageFields = [
+                'change_in_total_value_percentage',
+                'turnover',
+                'turnover_alt_sell',
+                'turnover_alt_buy'
+            ];
+        
+            foreach ($percentageFields as $field) {
+                if (isset($this->summary[$field])) {
+                    $this->summary[$field] = number_format($this->summary[$field], 2) . ' %';
+                }
+            }
+        }
+    }
+
+    public function updatedSelectedQuarter($quarter)
+    {
+        $this->loadFundData($quarter);
+    }
+
+    public function generateQuarters()
+    {
+        $quarters = [];
+        // Query the database for the oldest filing date
+        $oldestFilingDate = DB::connection('pgsql-xbrl')
+        ->table('filings_summary')
+        ->where('cik', '=', $this->cik)
+        ->min('date');
+    
+        if (!$oldestFilingDate) {
+            // No data in the database, you might want to handle this case differently
+            return [];
+        }
+    
+        // Extract the year from the oldest filing date
+        $startYear = Carbon::parse($oldestFilingDate)->year;
+        $currentYear = Carbon::now()->year;
+        $currentQuarter = Carbon::now()->quarter;
+        $currentDay = Carbon::now()->day;
+        $quarterEndDates = [
+            1 => '-03-31',
+            2 => '-06-30',
+            3 => '-09-30',
+            4 => '-12-31',
+        ];
+        $quarterEndDays = [
+            1 => 31,
+            2 => 30,
+            3 => 30,
+            4 => 31,
+        ];
+    
+        for ($year = $startYear; $year <= $currentYear; $year++) {
+            for ($quarter = 1; $quarter <= 4; $quarter++) {
+                if ($year == $currentYear && $quarter > $currentQuarter) {
+                    break;
+                }
+                // Don't include the current quarter if it's not over yet
+                if ($year == $currentYear && $quarter == $currentQuarter && $currentDay < $quarterEndDays[$quarter]) {
+                    continue;
+                }
+                $quarterEnd = $year . $quarterEndDates[$quarter];
+                $quarterText = 'Q' . $quarter . ' ' . $year;
+                $quarters[$quarterEnd] = $quarterText;
+            }
+        }
+    
+        return array_reverse($quarters, true);
     }
 
     public function render()
