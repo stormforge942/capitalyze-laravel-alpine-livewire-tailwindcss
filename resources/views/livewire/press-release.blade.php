f439b57b7ddab4d19db972f25075c3245ea5d022
ace95322 <ace95322@gmail.com>
2023-08-09 23:51:23 +0200

    fix table

--- resources/views/livewire/press-release.blade.php
+++ resources/views/livewire/press-release.blade.php
@@ -31,9 +31,9 @@
                                         </tr>
                                         @foreach($pressRelease as $release)
                                         <tr>
-                                            <td class="break-all px-2">{{ $release->title }}</td>
+                                            <td onclick="showModal(this)" class="break-all px-2 w-[80%] cursor-pointer text-blue-500 open-slide" data-value="{{ $release->text }}">{{ $release->title }}</td>
                                             <td class="px-2">{{ $release->date }}</td>
-                                            <td class="px-2"><a href="{{ $release->url }}" target="_blank">More Info</a></td>
+                                            <td class="px-2 text-blue-500"><a href="{{ $release->url }}" target="_blank">More Info</a></td>
                                         </tr>
                                         @endforeach
                                 </table>
@@ -43,6 +43,68 @@
                 </div>
             </div>
         </main>
+        <div class="relative z-[100] hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true" id="modal">
+            <!--
+                Background backdrop, show/hide based on modal state.
+
+                Entering: "ease-out duration-300"
+                From: "opacity-0"
+                To: "opacity-100"
+                Leaving: "ease-in duration-200"
+                From: "opacity-100"
+                To: "opacity-0"
+            -->
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+            <div class="fixed inset-0 z-10 overflow-y-auto">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <!--
+                    Modal panel, show/hide based on modal state.
+
+                    Entering: "ease-out duration-300"
+                    From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    To: "opacity-100 translate-y-0 sm:scale-100"
+                    Leaving: "ease-in duration-200"
+                    From: "opacity-100 translate-y-0 sm:scale-100"
+                    To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                -->
+                <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div class="sm:flex sm:items-start">
+                        <div class="mt-2">
+                            <p class="text-sm text-gray-500" id="modal-text">1</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button onclick="hideModal()" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Close</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
         <!-- Page Content End -->
     </div>
 </div>
+
+
+@push('scripts')
+<script>
+    function showModal(e) {
+        var value = e.dataset.value;
+        var elem = document.getElementById('modal');
+
+        elem.classList.add('block');
+        elem.querySelector('#modal-text').textContent = value;
+        elem.classList.remove('hidden');
+    }
+
+    function hideModal() {
+        var elem = document.getElementById('modal');
+
+        elem.classList.remove('block');
+        elem.querySelector('#modal-text').textContent = '';
+        elem.classList.add('hidden');
+    }
+</script>
+@endpush
