0dfc78f4c49574d9af104d87099ef19a0965f9f6
ace95322 <ace95322@gmail.com>
2023-08-26 10:22:47 +0200

    fix migration name - add kernel command

--- database/migrations/2023_08_25_152122_create_mutual_funds_table.php
+++ database/migrations/2023_08_25_152122_create_mutual_funds_table.php
@@ -4,21 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnsToMutualFundsTable extends Migration
+return new class extends Migration
 {
-public function up()
-{
-    Schema::table('mutual_funds', function (Blueprint $table) {
-        $table->string('fund_symbol')->nullable();
-        $table->string('series_id')->nullable();
-        $table->string('class_id')->nullable();
-    });
-}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mutual_funds', function (Blueprint $table) {
+            $table->string('fund_symbol')->nullable();
+            $table->string('series_id')->nullable();
+            $table->string('class_id')->nullable();
+        });
+    }
 
-public function down()
-{
-    Schema::table('mutual_funds', function (Blueprint $table) {
-        $table->dropColumn(['fund_symbol', 'series_id', 'class_id']);
-    });
-}
-}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mutual_funds', function (Blueprint $table) {
+            $table->dropColumn(['fund_symbol', 'series_id', 'class_id']);
+        });
+    }
+};
