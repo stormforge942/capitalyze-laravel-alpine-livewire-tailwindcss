0fc4c201f8f391fb5bb80f9934d6ab25d1bfedfc
ace95322 <ace95322@gmail.com>
2023-09-20 13:42:49 +0545

    fix hydration error

--- app/Http/Livewire/OtcMetrics.php
+++ app/Http/Livewire/OtcMetrics.php
@@ -8,42 +8,43 @@ use Illuminate\Support\Facades\DB;
 class OtcMetrics extends Component
 {
     public $otc;
-    public $table;
-    public $metrics;
-    public $segments;
-    public $navbar;
+    private $table;
+    private $metrics;
+    private $segments;
+    private $navbar;
     public $period;
-    public $subnavbar;
-    public $activeIndex = null;
-    public $activeSubIndex = null;
-    public $currentNavbar = '';
+    private $subnavbar;
+    private $activeIndex = null;
+    private $activeSubIndex = null;
+    private $currentNavbar = '';
 
     protected $listeners = ['tabClicked', 'tabSubClicked', 'periodChange'];
 
-    public function getMetrics() {
+    public function getMetrics()
+    {
         $dbResponse = DB::connection('pgsql-xbrl')
-        ->table('public.otc_statements')
-        ->select('json_result')
-        ->where('symbol', '=', $this->otc->symbol)
-        ->where('is_annual_report', '=', $this->period === 'annual')
-        ->orderBy('date', 'desc')
-        ->get()->toArray();
-        
+            ->table('public.otc_statements')
+            ->select('json_result')
+            ->where('symbol', '=', $this->otc->symbol)
+            ->where('is_annual_report', '=', $this->period === 'annual')
+            ->orderBy('date', 'desc')
+            ->get()->toArray();
+
         $cols = [];
         $rows = [];
-    
+
         $this->currentNavbar = substr($this->activeSubIndex, strpos($this->activeSubIndex, "-") + 1);
-    
-        foreach($dbResponse as $dbData) {
+
+        foreach ($dbResponse as $dbData) {
             $data = json_decode($dbData->json_result, true);
-            foreach($data as $date => $valueLevel0) {
+            foreach ($data as $date => $valueLevel0) {
                 $cols[$date] = [];
-                foreach($valueLevel0 as $valueLevel1) {
-                    foreach($valueLevel1 as $keyLevel2 => $valueLevel2) {
-                        foreach($valueLevel2 as $keyLevel3 => $valueLevel3) {
+                foreach ($valueLevel0 as $valueLevel1) {
+                    foreach ($valueLevel1 as $keyLevel2 => $valueLevel2) {
+                        foreach ($valueLevel2 as $keyLevel3 => $valueLevel3) {
                             $cols[$date][$keyLevel3] = $valueLevel3;
-                            if($this->currentNavbar == $keyLevel2) {
-                                if(!in_array($keyLevel3, $rows, true)){
+                            if ($this->currentNavbar == $keyLevel2) {
+                                if (!in_array($keyLevel3, $rows, true)) {
                                     $rows[] =  $keyLevel3;
                                 }
                             }
@@ -52,54 +53,56 @@ class OtcMetrics extends Component
                 }
             }
         }
-        
+
         $this->metrics = $cols;
         $this->segments = $rows;
     }
 
-    public function getNavbar() {  
+    public function getNavbar()
+    {
         $data = json_decode(DB::connection('pgsql-xbrl')
-        ->table('public.otc_statements')
-        ->where('symbol', '=', $this->otc->symbol)
-        ->value('json_result'), true);
-        
+            ->table('public.otc_statements')
+            ->where('symbol', '=', $this->otc->symbol)
+            ->value('json_result'), true);
+
 
         $navbar = [];
 
-        foreach($data as $groups) {
-            foreach($groups as $key => $value) {
+        foreach ($data as $groups) {
+            foreach ($groups as $key => $value) {
                 $navbar[$key] = [];
                 $this->activeIndex = ($this->activeIndex == null) ? $key : $this->activeIndex;
-                foreach($value as $sub => $_) {
+                foreach ($value as $sub => $_) {
                     $id = $key . "-" . $sub;
                     $this->activeSubIndex = ($this->activeSubIndex == null) ? $id : $this->activeSubIndex;
                     $navbar[$key][] = ['id' => $id, 'title' => $sub, 'selected' => ($id == $this->activeSubIndex)];
                 }
             }
         }
-    
+
         $this->navbar = $navbar;
         $this->emit('navbarUpdated', $this->navbar, $this->activeIndex, $this->activeSubIndex);
     }
 
-    public function renderTable() {
+    public function renderTable()
+    {
         $i = 0;
         $class = '';
         $table = '<table class="table-auto min-w-full data border-collapse"><thead><tr>';
         $table .= '<th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900 bg-blue-300">Date</th>';
-        foreach(array_keys($this->metrics) as $date) {
-            $table .= '<th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-slate-950 bg-blue-300">'.$date.'</th>';
+        foreach (array_keys($this->metrics) as $date) {
+            $table .= '<th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-slate-950 bg-blue-300">' . $date . '</th>';
         }
         $table .= '</thead><tbody class="divide-y bg-white">';
-        foreach($this->segments as $segment) {
+        foreach ($this->segments as $segment) {
             $class = ($i % 2 == 0) ? 'class="border border-slate-50 bg-cyan-50 hover:bg-blue-200 dark:bg-slate-700 dark:odd:bg-slate-800 dark:odd:hover:bg-slate-900 dark:hover:bg-slate-700"' : 'class="border border-slate-50 bg-white border-slate-100 dark:border-slate-400 hover:bg-blue-200 dark:bg-slate-700 dark:odd:bg-slate-800 dark:odd:hover:bg-slate-900 dark:hover:bg-slate-700"';
-            $table .= '<tr '.$class.'><td class="break-words max-w-[150px] lg:max-w-[400px] px-2 py-2 text-sm text-gray-900 font-bold">'.preg_replace('/(?<=\w)(?=[A-Z])/', ' ', $segment).'</td>';
-            foreach(array_keys($this->metrics) as $date) {
-                if(array_key_exists($segment, $this->metrics[$date])) {
+            $table .= '<tr ' . $class . '><td class="break-words max-w-[150px] lg:max-w-[400px] px-2 py-2 text-sm text-gray-900 font-bold">' . preg_replace('/(?<=\w)(?=[A-Z])/', ' ', $segment) . '</td>';
+            foreach (array_keys($this->metrics) as $date) {
+                if (array_key_exists($segment, $this->metrics[$date])) {
                     $data = $this->metrics[$date][$segment];
                     $dataSend = array("symbol" => $this->otc->symbol, "source" => 'public.otc_statements', "date" => $date, "value" => $data);
                     $data_json = json_encode($dataSend);
-                    $table .= '<td data-value="'.htmlspecialchars($data_json).'" class="whitespace-nowrap px-2 py-2 text-sm text-gray-900 hover:cursor-pointer hover:underline underline-offset-1 open-slide">'.$data.'</td>';
+                    $table .= '<td data-value="' . htmlspecialchars($data_json) . '" class="whitespace-nowrap px-2 py-2 text-sm text-gray-900 hover:cursor-pointer hover:underline underline-offset-1 open-slide">' . $data . '</td>';
                 } else {
                     $table .= '<td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900"></td>';
                 }
@@ -110,7 +113,7 @@ class OtcMetrics extends Component
         $table .= '</tbody></table>';
 
         $this->table = $table;
-   }
+    }
 
     public function mount($otc, $period)
     {
@@ -121,13 +124,15 @@ class OtcMetrics extends Component
         $this->renderTable();
     }
 
-    public function tabClicked($key) {
+    public function tabClicked($key)
+    {
         $this->activeIndex = $key;
         $this->getMetrics();
         $this->renderTable();
     }
 
-    public function tabSubClicked($key) {
+    public function tabSubClicked($key)
+    {
         $this->activeSubIndex = $key;
         $this->getMetrics();
         $this->renderTable();
@@ -135,6 +140,17 @@ class OtcMetrics extends Component
 
     public function render()
     {
-        return view('livewire.otc-metrics');
+        // to fix hydration error we are manually passing the data
+        return view('livewire.otc-metrics', [
+            'otc' => $this->otc,
+            'table' => $this->table,
+            'metrics' => $this->metrics,
+            'segments' => $this->segments,
+            'navbar' => $this->navbar,
+            'subnavbar' => $this->subnavbar,
+            'activeIndex' => $this->activeIndex,
+            'activeSubIndex' => $this->activeSubIndex,
+            'currentNavbar' => $this->currentNavbar,
+        ]);
     }
 }
