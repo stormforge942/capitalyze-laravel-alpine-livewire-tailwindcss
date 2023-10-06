62ff77093722e1b1e16f594d6962de9c41e5337c
ace95322 <ace95322@gmail.com>
2023-10-07 00:32:25 +0545

    extract japan

---
+++ app/Http/Livewire/Japan/FilingsTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Livewire\Japan;
+
+use App\Models\JapanFilings;
+use App\Http\Livewire\BaseFilingsTable;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\PowerGrid;
+use PowerComponents\LivewirePowerGrid\Filters\Filter;
+use PowerComponents\LivewirePowerGrid\PowerGridEloquent;
+
+class FilingsTable extends BaseFilingsTable
+{
+    public function data(): ?Builder
+    {
+        return JapanFilings::query()
+            ->where('symbol', '=', $this->model->symbol);
+    }
+
+    public function addColumns(): PowerGridEloquent
+    {
+        return PowerGrid::eloquent()
+            ->addColumn('industry')
+            ->addColumn('fiscal_year')
+            ->addColumn('fiscal_period')
+            ->addColumn('updated_at')
+            ->addColumn('url', function (JapanFilings $japanFilings) {
+                return ('<a class="text-blue-500" target="_blank"  href="'.$japanFilings->url.'">More Info</a>');
+            });
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Industry', 'industry')->sortable(),
+            Column::make('Fiscal Year', 'fiscal_year')->sortable(),
+            Column::make('Fiscal Period', 'fiscal_period')->sortable(),
+            Column::make('Updated At', 'updated_at')->sortable(),
+            Column::make('URL', 'url')->sortable(),
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [
+            Filter::inputText('industry', 'industry')->operators([]),
+            Filter::inputText('fiscal_year', 'fiscal_year')->operators([]),
+            Filter::inputText('fiscal_period', 'fiscal_period')->operators([]),
+            Filter::inputText('updated_at', 'updated_at')->operators([]),
+            Filter::inputText('url', 'url')->operators([]),
+        ];
+    }
+}
---
+++ app/Http/Livewire/Japan/Navbar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Livewire\Japan;
+
+use App\Http\Livewire\BaseNavbar;
+
+class Navbar extends BaseNavbar
+{
+    public function bottomNavKey(): string {
+        return 'japan.';
+    }
+}
