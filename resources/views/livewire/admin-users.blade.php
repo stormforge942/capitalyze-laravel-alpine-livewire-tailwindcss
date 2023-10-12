b9f174a5b6215050e77fc9676d4d86239b168b7e
ace95322 <ace95322@gmail.com>
2023-10-12 17:09:05 +0545

    add logo in admin

--- resources/views/livewire/admin-users.blade.php
+++ resources/views/livewire/admin-users.blade.php
@@ -1,104 +1,101 @@
-<div class="py-12">
-    <div class="mx-auto flex">
-        <div class="mt-4 px-4 sm:px-6 lg:px-8 bg-white py-4 shadow rounded w-full md:w-2/3 md:mx-auto">
-            <div class="sm:flex sm:items-start flex-col">
-                <div class="block">
-                    <h1 class="text-base font-semibold leading-6 text-gray-900">User management</h1>
-                </div>
+<div>
+    <div class="px-4 sm:px-6 lg:px-8 bg-white py-4 shadow rounded max-w-5xl mx-auto">
+        <div class="sm:flex sm:items-start flex-col">
+            <div class="block">
+                <h1 class="text-base font-semibold leading-6 text-gray-900">User management</h1>
             </div>
-            <div class="mt-8 flow-root rounded-lg overflow-x-auto ">
-                <table class="table-auto min-w-full border-collapse text-left" x-data>
-                    <thead>
-                        <tr>
-                            <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
-                                    wire:click.prevent="sortBy('id')">ID</a></th>
-                            <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
-                                    wire:click.prevent="sortBy('name')">Name</a></th>
-                            <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
-                                    wire:click.prevent="sortBy('email')">Email</a></th>
-                            <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
-                                    wire:click.prevent="sortBy('email')">Is Admin</a></th>
-                            <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
-                                    wire:click.prevent="sortBy('email')">Group</a></th>
-                            <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
-                                    wire:click.prevent="sortBy('approved')">Approved</a></th>
-                            <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
-                                    wire:click.prevent="sortBy('created_at')">Registration Date</a></th>
-                            <th colspan="2" class="capitalize p-2 whitespace-nowrap bg-gray-100">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-sm">
-                        @foreach ($users as $index => $user)
-                            <tr class="{{ $index % 2 ? 'bg-gray-200' : '' }}"> <!-- Alternating row colors -->
-                                <td class="whitespace-nowrap p-2">{{ $user->id }}</td>
-                                <td class="whitespace-nowrap p-2">{{ $user->name }}</td>
-                                <td class="whitespace-nowrap p-2">{{ $user->email }}</td>
-                                <td class="p-2">
-                                    <input type="checkbox" class="disabled:opacity-50"
-                                        @change="$wire.updateIsAdmin({{ $user->id }}, $el.checked)"
-                                        @if ($user->is_admin) checked @endif
-                                        @if ($user->id === auth()->id()) disabled @endif>
-                                </td>
-                                <td class="p-2">
-                                    <select class="text-sm px-2 py-1 outline outline-1 outline-gray-500"
-                                        wire:change="updateUserGroup('{{ $user->id }}', $event.target.value)">
-                                        @foreach ($groups as $group)
-                                            <option value="{{ $group->id }}"
-                                                @if ($user->group_id == $group->id) selected @endif>{{ $group->name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </td>
-                                <td class="p-2">{{ $user->is_approved ? 'Yes' : 'No' }}</td>
-                                <td class="p-2">{{ $user->created_at->format('m/d/Y') }}</td>
-                                <td class="p-2">
-                                    @if (!$user->is_approved)
-                                        <button wire:click="approveUser({{ $user->id }})"
-                                            class="mx-auto bg-green-500 text-white py-1 px-3 rounded hover:bg-green-600 flex items-center gap-2 whitespace-nowrap">
-                                            <!-- Green Approve button -->
-                                            <svg class="inline w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                class="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M4.5 12.75l6 6 9-13.5" />
-                                            </svg> Approve
-                                        </button>
-                                    @else
-                                        <button wire:click="disapproveUser({{ $user->id }})"
-                                            class="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600 flex items-center gap-2 whitespace-nowraps">
-                                            <!-- Red Disapprove button -->
-                                            <svg class="inline w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                class="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M6 18L18 6M6 6l12 12" />
-                                            </svg>
-                                            Disapprove
-                                        </button>
-                                    @endif
-                                </td>
-                                <td>
-                                    <button wire:click="deleteUser({{ $user->id }})"
-                                        class="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600 flex items-center gap-2 whitespace-nowrap">
+        </div>
+        <div class="mt-8 flow-root rounded-lg overflow-x-auto">
+            <table class="table-auto min-w-full border-collapse text-left" x-data>
+                <thead>
+                    <tr>
+                        <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
+                                wire:click.prevent="sortBy('id')">ID</a></th>
+                        <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
+                                wire:click.prevent="sortBy('name')">Name</a></th>
+                        <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
+                                wire:click.prevent="sortBy('email')">Email</a></th>
+                        <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
+                                wire:click.prevent="sortBy('email')">Is Admin</a></th>
+                        <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
+                                wire:click.prevent="sortBy('email')">Group</a></th>
+                        <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
+                                wire:click.prevent="sortBy('approved')">Approved</a></th>
+                        <th class="capitalize p-2 whitespace-nowrap bg-gray-100"><a href="#"
+                                wire:click.prevent="sortBy('created_at')">Registration Date</a></th>
+                        <th colspan="2" class="capitalize p-2 whitespace-nowrap bg-gray-100">Action</th>
+                    </tr>
+                </thead>
+                <tbody class="text-sm">
+                    @foreach ($users as $index => $user)
+                        <tr class="{{ $index % 2 ? 'bg-gray-200' : '' }}"> <!-- Alternating row colors -->
+                            <td class="whitespace-nowrap p-2">{{ $user->id }}</td>
+                            <td class="whitespace-nowrap p-2">{{ $user->name }}</td>
+                            <td class="whitespace-nowrap p-2">{{ $user->email }}</td>
+                            <td class="p-2">
+                                <input type="checkbox" class="disabled:opacity-50"
+                                    @change="$wire.updateIsAdmin({{ $user->id }}, $el.checked)"
+                                    @if ($user->is_admin) checked @endif
+                                    @if ($user->id === auth()->id()) disabled @endif>
+                            </td>
+                            <td class="p-2">
+                                <select class="text-sm px-2 py-1 outline outline-1 outline-gray-500"
+                                    wire:change="updateUserGroup('{{ $user->id }}', $event.target.value)">
+                                    @foreach ($groups as $group)
+                                        <option value="{{ $group->id }}"
+                                            @if ($user->group_id == $group->id) selected @endif>{{ $group->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td class="p-2">{{ $user->is_approved ? 'Yes' : 'No' }}</td>
+                            <td class="p-2">{{ $user->created_at->format('m/d/Y') }}</td>
+                            <td class="p-2 text-left">
+                                @if (!$user->is_approved)
+                                    <button wire:click="approveUser({{ $user->id }})"
+                                        class="w-full bg-green-500 text-white py-1 px-3 rounded hover:bg-green-600 flex justify-center items-center gap-2 whitespace-nowrap">
+                                        <!-- Green Approve button -->
+                                        <svg class="inline w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M4.5 12.75l6 6 9-13.5" />
+                                        </svg> Approve
+                                    </button>
+                                @else
+                                    <button wire:click="disapproveUser({{ $user->id }})"
+                                        class="w-full bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600 flex justify-center items-center gap-2 whitespace-nowraps">
+                                        <!-- Red Disapprove button -->
                                         <svg class="inline w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
                                             viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                             class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M6 18L18 6M6 6l12 12" />
                                         </svg>
-                                        Delete
+                                        Disapprove
                                     </button>
+                                @endif
+                            </td>
+                            <td>
+                                <button wire:click="deleteUser({{ $user->id }})"
+                                    class="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600 flex items-center gap-2 whitespace-nowrap">
+                                    <svg class="inline w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                    Delete
+                                </button>
 
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $users->links() }}
-            </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="mt-3">
+            {{ $users->links() }}
         </div>
     </div>
-
 
     <!-- Delete User Confirmation Modal -->
     <x-jet-dialog-modal wire:model="confirmingUserDeletion" wire:ignore>
@@ -131,6 +128,3 @@
         </x-slot>
     </x-jet-dialog-modal>
 </div>
-@push('scripts')
-    <script></script>
-@endpush
