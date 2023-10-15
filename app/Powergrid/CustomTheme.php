06ae841042a6c0f7506af5d5b7428eb592674c7c
ace95322 <ace95322@gmail.com>
2023-10-16 04:30:23 +0545

    fix

--- app/Powergrid/CustomTheme.php
+++ app/Powergrid/CustomTheme.php
@@ -15,7 +15,7 @@ class CustomTheme extends Tailwind
         return Theme::table('w-full text-right bg-white rounded-md overflow-clip')
             ->div('my-3 bg-white shadow-lg rounded-lg relative')
             ->thead('font-sm font-semibold capitalize bg-[#E6E6E680] rounded-t-md')
-            ->th('pl-6 py-2 text-dark whitespace-nowrap first:text-left last:pr-6')
+            ->th('pl-6 py-2 text-dark whitespace-nowrap first:[&>div]:justify-start first:text-left last:pr-6')
             ->tbody('divide-y-2')
             ->tdBody('pl-6 py-4 whitespace-nowrap first:text-left last:pr-6');
     }
--- resources/views/vendor/livewire-powergrid/components/cols.blade.php
+++ resources/views/vendor/livewire-powergrid/components/cols.blade.php
@@ -16,37 +16,36 @@
     @if ($column->sortable) x-data x-multisort-shift-click="{{ $this->id }}" @endif
     style="{{ $column->hidden === true ? 'display:none' : '' }}; width: max-content; @if ($column->sortable) cursor:pointer; @endif {{ $theme->table->thStyle . ' ' . $column->headerStyle }}">
     <div @class([
+        'flex justify-end items-center gap-1',
         $theme->cols->divClass,
     ]) style="{{ $theme->cols->divStyle }}" @if ($column->sortable)
         wire:click="sortBy('{{ $field }}')"
         @endif>
-        <div class="flex items-center gap-1">
-            <span>{{ $column->title }}</span>
-            @if ($column->sortable)
-                <span>
-                    @if ($multiSort && array_key_exists($field, $sortArray))
-                        @if ($sortArray[$field] == 'desc')
-                            &#8595;
-                        @else
-                            &#8593;
-                        @endif
-                    @elseif($multiSort)
-                        &#8597;
+        <span>{{ $column->title }}</span>
+        @if ($column->sortable)
+            <span>
+                @if ($multiSort && array_key_exists($field, $sortArray))
+                    @if ($sortArray[$field] == 'desc')
+                        &#8595;
                     @else
-                        @if ($sortField !== $field)
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                viewBox="0 0 16
-                    16" fill="none">
-                                <path d="M12 6L8 2L4 6H12ZM12 10L8 14L4 10H12Z" fill="#464E49" />
-                            </svg>
-                        @elseif ($sortDirection == 'desc')
-                            &#8593;
-                        @else
-                            &#8595;
-                        @endif
+                        &#8593;
                     @endif
-                </span>
-            @endif
-        </div>
+                @elseif($multiSort)
+                    &#8597;
+                @else
+                    @if ($sortField !== $field)
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 16
+                    16" fill="none">
+                            <path d="M12 6L8 2L4 6H12ZM12 10L8 14L4 10H12Z" fill="#464E49" />
+                        </svg>
+                    @elseif ($sortDirection == 'desc')
+                        &#8593;
+                    @else
+                        &#8595;
+                    @endif
+                @endif
+            </span>
+        @endif
     </div>
 </th>
