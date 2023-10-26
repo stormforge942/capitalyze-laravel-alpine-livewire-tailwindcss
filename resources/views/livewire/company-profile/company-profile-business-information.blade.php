f7956a2ca692066dfcdffb3d7fa056ad2ade009d
ace95322 <ace95322@gmail.com>
2023-10-26 23:43:55 +0500

    fixes

--- resources/views/livewire/company-profile/company-profile-business-information.blade.php
+++ resources/views/livewire/company-profile/company-profile-business-information.blade.php
@@ -21,85 +21,72 @@
         <div class="bussiness-information-left order-2 lg:order-1 basis-full  lg:basis-3/4 ">
 
             <div class="white-card">
-                <p class="title">
-                    Business
-                </p>
-                <div class="subtitle">
-                    <div>Business Description</div>
+                @php
+                $businessContent = $menuLinks['business'];
+                $businessContent = preg_replace('/\s+/', ' ', $businessContent);
+                $businessContent = str_replace('\n', '', $businessContent);
+                $businessContent = str_replace('\t', '', $businessContent);
+                $businessContent = preg_replace('/class="(.?)+"/i','', $businessContent);
+                $businessContent = preg_replace('/business(?=\<) /i', "<p class='title'>Business</p>" , $businessContent, 1);
+                $businessContent=preg_replace('/business</i', "<p class='title'>Business</p><" , $businessContent, 1);
+                $businessContent=preg_replace('/>company background</i', "><p class='subtitle'>Company Background</p><" ,$businessContent, 1);
+                $businessContent=preg_replace('/>products</i', "><p class='subtitle'>Products</p><" ,$businessContent, 1);
+                $businessContent=preg_replace('/>services</i', "><p class='subtitle'>Services</p><" , $businessContent, 1);
+                $businessContent=preg_replace('/>markets and distribution</i', "><p class='subtitle'>Markets and Distribution</p><" , $businessContent,1);
+                $businessContent=preg_replace('/>competition</i', "><p class='subtitle'>Competition</p><" ,$businessContent, 1);
+                $businessContent=preg_replace('/>supply of components</i', "><p class='subtitle'>Supply of Components</p><" , $businessContent, 1);
+                $businessContent=preg_replace('/>research and development</i', "><p class='subtitle'>Research and Development</p><" , $businessContent, 1);
+                $businessContent=preg_replace('/>intellectual property</i', "><p class='subtitle'>Intellectual Property</p><" , $businessContent, 1);
+                $businessContent=preg_replace('/>business seasonality and product introductions</i', "><p class='subtitle'>Business Seasonality and Product Introductions</p><" , $businessContent, 1);
+                $businessContent=preg_replace('/>human capital</i', "><p class='subtitle'>Human Capital</p><" ,$businessContent, 1);
+                $businessContent=preg_replace('/>available information</i', "><p class='subtitle'>Available Information</p><" , $businessContent, 1);
+                @endphp
+                    {!!$businessContent!!}
                 </div>
-                <div class="text-info">
-                    {{ @$profile['description'] ?: 'Description not found' }}
-                </div>
-                <p class="subtitle">
-                    Company Background
-                </p>
-                <p class="text-info">
-                    Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft. Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft. Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft.
-                </p>
-                <p class="subtitle">
-                    Products
-                </p>
-                <p class="text-info">
-                    Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft. Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft. Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft.
-                </p>
-                <p class="subtitle">
-                    Services
-                </p>
-                <p class="text-info">
-                    Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft. Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft. Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, with US$394.3 billion in 2022 revenue. As of March 2023, Apple is the world's biggest company by market capitalisation. As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta Platforms, and Microsoft.
-                </p>
-                {{-- <div class="text">
-                    {{ @$profile['description'] ?: 'Description not found' }}
-                </div> --}}
-            </div>
-
-
-      </div>
-      <div class="bussiness-information-right order-1 lg:order-2 basis-full lg:basis-1/4">
-
-        <div class="white-card">
-
-            <ul class="list-items">
-                <li>
-                    <a href="#" class="active">Company Background</a>
-                </li>
-                <li>
-                    <a href="#">Products</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Markets and Distribution</a>
-                </li>
-                <li>
-                    <a href="#">Competition</a>
-                </li>
-                <li>
-                    <a href="#">Supply of Components</a>
-                </li>
-                <li>
-                    <a href="#">Research and Development</a>
-                </li>
-                <li>
-                    <a href="#">Intellectual Properties</a>
-                </li>
-                <li>
-                    <a href="#">Business Seasonality and Product Introductions</a>
-                </li>
-                <li>
-                    <a href="#">Human Capital</a>
-                </li>
-            </ul>
-            <div class="flex justify-end">
-                <span class="business-footer-text">
-                    Source: FY 2022 10-k
-                </span>
             </div>
         </div>
+        <div class="bussiness-information-right order-1 lg:order-2 basis-full lg:basis-1/4">
+
+            <div class="white-card">
+
+                <ul class="list-items">
+                    <li>
+                        <a href="#" class="active">Company Background</a>
+                    </li>
+                    <li>
+                        <a href="#">Products</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Markets and Distribution</a>
+                    </li>
+                    <li>
+                        <a href="#">Competition</a>
+                    </li>
+                    <li>
+                        <a href="#">Supply of Components</a>
+                    </li>
+                    <li>
+                        <a href="#">Research and Development</a>
+                    </li>
+                    <li>
+                        <a href="#">Intellectual Properties</a>
+                    </li>
+                    <li>
+                        <a href="#">Business Seasonality and Product Introductions</a>
+                    </li>
+                    <li>
+                        <a href="#">Human Capital</a>
+                    </li>
+                </ul>
+                <div class="flex justify-end">
+                    <span class="business-footer-text">
+                        Source: FY 2022 10-k
+                    </span>
+                </div>
             </div>
-</div>
-
-
-
+        </div>
+    </div>
 </div>
