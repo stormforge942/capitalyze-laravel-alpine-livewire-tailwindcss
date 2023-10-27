073645cc75aaad2c7d60a5cac856a732ac7dab64
ace95322 <ace95322@gmail.com>
2023-10-27 17:47:18 +0545

    fixed all the responsive issue in tab and other issue..

--- resources/views/livewire/comapny-ownership.blade.php
+++ resources/views/livewire/comapny-ownership.blade.php
@@ -10,7 +10,7 @@
                     <span class="sr-only">Loading...</span>
                 </div>
             </div>
-            <div class="sm:flex sm:items-start flex-col company-profile-page mt-0 mx-0" wire:loading.remove>
+            <div class="flex sm:items-start flex-col company-profile-page m-2" wire:loading.remove>
                 <div>
                     <div class="page-titles flex justify-start content-start mb-0 mt-2 mx-2">
                         <div> <h4 class="font-sm font-semi-bold pr-3 mt-2 mb-2">Track Investors</h4></div>
@@ -18,23 +18,69 @@
                         <div> <h5 class="font-sm font-semi-bold pr-3 mt-2 mb-2">Discover</h5></div>
                     </div> 
                 </div>   
-                <div class="tabs-wrapper flex">
+                <div class="hidden md:flex tabs-wrapper w-full">
                     <div @class(['tab', 'active' => $infoTabActive == 'discover']) wire:click="setInfoActiveTab('discover')">Discover</div>
                     <div @class(['tab', 'active' => $infoTabActive == 'my-favorites']) wire:click="setInfoActiveTab('my-favorites')">My Favorites</div>
                 </div>
-                @if(!$loading)
-                    <div class="">
-                        No data is loding
+                <div class="hidden md:flex content-between items-center justify-between w-full">
+                    <div class=" title font-semibold">Discover Funds</div>
+                    <div class="search mr-2">
+                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-400">Search</label>
+                        <div class="relative p-0 m-0 sm:mt-3 md:mt-3">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                </svg>
+                            </div>
+                            <input type="search" id="default-search" class="block h-0 leading-3 w-full p-4 pl-10 text-sm text-gray-900 border-b-2 border-t-0 border-l-0 border-r-0 bg-gray-100 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Search funds..." required>
+                        </div>
                     </div>
-                @else
-                    @if ($infoTabActive == 'discover')
-                    <div>
-                        <livewire:discover.fund/>
+                </div>
+                <div class="flex md:hidden justify-between relative w-full mt-5 mx-3" x-data="{dropdownMenu: false}" @keydown.window.escape="dropdownMenu = false" @click.away="dropdownMenu = false">
+                    <button @click="dropdownMenu = ! dropdownMenu" class="flex items-center p-2 bg-[#52D3A2]  rounded">
+                        <span class="mr-4 font-normal text-sm p-x-4">{{$infoTabActive == 'discover' ? 'Discover' : 'My Favorites'}} </span>
+                        <svg class="w-5 h-5 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                    <div x-show="dropdownMenu" class="absolute left-0 py-2 mt-2 bg-white  rounded-md shadow-xl w-44">
+                        <a href="javascript;" wire:click.prevent="setInfoActiveTab('discover')" class="block px-4 py-2 text-sm text-gray-300 text-gray-700 hover:bg-gray-400 hover:text-white">
+                            Discover
+                        </a>
+                        <a href="javascript;" wire:click.prevent="setInfoActiveTab('my-favorites')" class="block px-4 py-2 text-sm text-gray-300 text-gray-700 hover:bg-gray-400 hover:text-white">
+                            My Favorites
+                        </a>
                     </div>
-                    @elseif($infoTabActive == 'my-favorites')
-                        <livewire:discover.favorites/>
+                    <div class="search mr-5">
+                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-400">Search</label>
+                        <div class="relative p-0 m-0 sm:mt-3 md:mt-3">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                </svg>
+                            </div>
+                            <input type="search" id="default-search" class="block h-0 leading-3 w-full p-4 pl-10 text-sm text-gray-900 border-b-2 border-t-0 border-l-0 border-r-0 bg-gray-100 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Search funds..." required>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-10 w-full">
+                    @if(!$loading)
+                        <div class="">
+                            No data is loding
+                        </div>
+                    @else
+                        @if ($infoTabActive == 'discover')
+                        <div>
+                            <livewire:discover.fund/>
+                        </div>
+                        @elseif($infoTabActive == 'my-favorites')
+                            <livewire:discover.favorites/>
+                        @endif
                     @endif
-                @endif
+                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/discover/fund.blade.php
+++ resources/views/livewire/discover/fund.blade.php
@@ -1,23 +1,9 @@
-<div class="discover flex-1 mb-10">
-    <div class="flex flex-col md:flex-row content-between items-center justify-between">
-        <div class="title font-semibold">Discover Funds</div>
-        <div class="search">
-            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div class="relative p-0 m-0 sm:mt-3 md:mt-3">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
-                </div>
-                <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border-b-2 border-t-0 border-l-0 border-r-0 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Discover items..." required>
-            </div>
-        </div>
-    </div>
+<div class="discover flex flex-col">
     @if($loading)
         <div class="flex-1 mt-6 ml-0 mr-0">
             <div class="grid gap-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
                 @foreach($discover as $item)
-                    <div class="box shadow-md bg-[#ffffff] p-3 m-2 border-r-0.5">
+                    <div class="box shadow-md bg-[#ffffff] p-3 m-2 border-0.5 rounded border-[#D4DDD7]">
                         <div class="flex content-center justify-between items-center">
                             <div>
                                 <h4 class="text-sm font-semibold text-[#3561E7]">{{$item['title']}}</h4>
@@ -28,7 +14,7 @@
                                 </svg>
                             </div>
                         </div>
-                        <div class="grid grid-cols-4 gap-y-3 md:grid-cols-2">
+                        <div class="grid grid-cols-4 gap-y-0 md:grid-cols-2">
                             @foreach($item['items'] as $value)
                                 <div class="m-0 p-2">
                                     <label class="font-normal text-xs">{{$value['label']}}</label>
