0060112c00a180a992c73a966ce79b26f9a8ca0b
ace95322 <ace95322@gmail.com>
2023-11-04 10:18:39 +0100

    change query

--- app/Http/Livewire/TrackInvestor/Fund.php
+++ app/Http/Livewire/TrackInvestor/Fund.php
@@ -17,11 +17,18 @@ class Fund extends Component
     public function render()
     {
         $data = DB::connection('pgsql-xbrl')
-        ->table('filings_summary')
-        ->select('investor_name','total_value','portfolio_size','change_in_total_value')
-        ->where('investor_name','=', 'Intergy Private Wealth, LLC' )
-        ->orderBy('date', 'desc')
-        ->get();
+            ->table('filings_summary')
+            ->select('fs.investor_name', 'fs.total_value', 'fs.portfolio_size', 'fs.change_in_total_value', 'latest_dates.max_date')
+            ->from('filings_summary as fs')
+            ->join(DB::raw('(SELECT investor_name, MAX(date) AS max_date FROM filings_summary GROUP BY investor_name) as latest_dates'), function($join)
+            {
+                $join->on('fs.investor_name', '=', 'latest_dates.investor_name');
+                $join->on('fs.date', '=', 'latest_dates.max_date');
+            })
+            ->get();
+    
+    
+
         $this->loading = true;
         return view('livewire.track-investor.fund', [
             'investors' => $data
