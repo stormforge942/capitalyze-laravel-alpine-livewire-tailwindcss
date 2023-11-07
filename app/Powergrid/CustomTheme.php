34b0b632f0fa07430553b429f37d5dcce1f458c1
ace95322 <ace95322@gmail.com>
2023-11-07 23:25:08 +0545

    fix table head color

--- app/Powergrid/CustomTheme.php
+++ app/Powergrid/CustomTheme.php
@@ -17,7 +17,7 @@ class CustomTheme extends Tailwind
     {
         return Theme::table('w-full text-right bg-white rounded-md overflow-clip')
             ->div('my-3 bg-white shadow-lg rounded-lg relative')
-            ->thead('font-sm font-semibold capitalize bg-[#E6E6E680] rounded-t-md')
+            ->thead('font-sm font-semibold capitalize bg-[#EDEDED] rounded-t-md')
             ->th('pl-6 py-2 text-dark whitespace-nowrap first:flex first:text-left last:pr-6')
             ->tbody('divide-y-2')
             ->tdBody('pl-6 py-4 whitespace-nowrap first:text-left last:pr-6')
