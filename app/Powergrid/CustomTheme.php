2906c6d5c5f6db33a882612d006538bae6694bcc
ace95322 <ace95322@gmail.com>
2023-11-15 02:37:29 +0545

    fix table box shadow

--- app/Powergrid/CustomTheme.php
+++ app/Powergrid/CustomTheme.php
@@ -16,7 +16,7 @@ class CustomTheme extends Tailwind
     public function table(): Table
     {
         return Theme::table('w-full bg-white rounded-md overflow-clip')
-            ->div('my-3 bg-white shadow-lg rounded-lg relative')
+            ->div('my-3 bg-white rounded-lg relative', 'box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.03)')
             ->thead('font-sm font-semibold capitalize bg-[#EDEDED] rounded-t-md')
             ->th('pl-6 py-2 text-dark whitespace-nowrap last:pr-6')
             ->tbody('divide-y-2')
