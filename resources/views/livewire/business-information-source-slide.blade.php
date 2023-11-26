f8effbf944378f0513ffa22740a487fb76e33896
ace95322 <ace95322@gmail.com>
2023-11-26 11:26:18 +0545

    fix

--- resources/views/livewire/business-information-source-slide.blade.php
+++ resources/views/livewire/business-information-source-slide.blade.php
@@ -1,7 +1,7 @@
 <x-wire-elements-pro::tailwind.slide-over>
     <div wire:init="load">
         <div class="place-items-center" wire:loading.grid>
-            <span class="mx-auto simple-loader !text-blue-dark"></span>
+            <span class="mx-auto simple-loader text-blue"></span>
         </div>
 
         <div wire:loading.remove>
