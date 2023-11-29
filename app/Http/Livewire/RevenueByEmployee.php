652d156268f4c710a01eb7ae09cd5832065edb5f
ace95322 <ace95322@gmail.com>
2023-11-29 20:35:05 +0500

    fix code issue in the mount

--- app/Http/Livewire/RevenueByEmployee.php
+++ app/Http/Livewire/RevenueByEmployee.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\EodPrices;
 use App\Models\InfoTikrPresentation;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
@@ -100,20 +101,20 @@ class RevenueByEmployee extends Component
 
     public function mount(Request $request, $company, $ticker, $period)
     {
-        $first = DB::connection('pgsql-xbrl')
-            ->table('eod_prices')
-            ->where('symbol', strtolower($this->ticker))
-            ->latest('date')->first()?->adj_close;
-        $previous = DB::connection('pgsql-xbrl')
-            ->table('eod_prices')
-            ->where('symbol', strtolower($this->ticker))
+        $eodPrices = EodPrices::where('symbol', strtolower($this->company->ticker))
             ->latest('date')
-            ->skip(1)->first()?->adj_close;
-        if ($previous && $first) {
-            $this->dynamic = round((($first - $previous) / $previous) * 100, 2);
+            ->take(2)
+            ->pluck('adj_close')
+            ->toArray();
+
+        $latestPrice = $eodPrices[0] ?? 0;
+        $previousPrice = $eodPrices[1] ?? 0;
+
+        if ($latestPrice > 0 && $previousPrice > 0) {
+            $this->dynamic = round((($latestPrice - $previousPrice) / $previousPrice) * 100, 2);
         }
 
-        $this->cost =  $first;
+        $this->cost =  $latestPrice;
         $this->company = $company;
         $this->ticker = $ticker;
         $this->period = $period;
--- app/Http/Livewire/RevenueByGeography.php
+++ app/Http/Livewire/RevenueByGeography.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\EodPrices;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
@@ -97,20 +98,20 @@ class RevenueByGeography extends Component
 
     public function mount(Request $request, $company, $ticker, $period)
     {
-        $first = DB::connection('pgsql-xbrl')
-            ->table('eod_prices')
-            ->where('symbol', strtolower($this->ticker))
-            ->latest('date')->first()?->adj_close;
-        $previous = DB::connection('pgsql-xbrl')
-            ->table('eod_prices')
-            ->where('symbol', strtolower($this->ticker))
+        $eodPrices = EodPrices::where('symbol', strtolower($this->company->ticker))
             ->latest('date')
-            ->skip(1)->first()?->adj_close;
-        if ($previous && $first) {
-            $this->dynamic = round((($first - $previous) / $previous) * 100, 2);
+            ->take(2)
+            ->pluck('adj_close')
+            ->toArray();
+
+        $latestPrice = $eodPrices[0] ?? 0;
+        $previousPrice = $eodPrices[1] ?? 0;
+
+        if ($latestPrice > 0 && $previousPrice > 0) {
+            $this->dynamic = round((($latestPrice - $previousPrice) / $previousPrice) * 100, 2);
         }
 
-        $this->cost =  $first;
+        $this->cost =  $latestPrice;
         $this->company = $company;
         $this->ticker = $ticker;
         $this->period = $period;
