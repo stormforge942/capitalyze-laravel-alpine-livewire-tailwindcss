2d2fe303b8b5eb1f16ebf6302b5bb21bcaa8c997
ace95322 <ace95322@gmail.com>
2023-11-30 23:34:26 +0500

    fix filter reopen issue

--- resources/views/livewire/company-report.blade.php
+++ resources/views/livewire/company-report.blade.php
@@ -106,8 +106,8 @@
                                     <div class="select-wrapper flex gap-x-4 items-center custom-text-xs">
                                         <div class=" flex items-center text-sm" x-data="{view: null, viewTypeSelectorOpen: false}" @click.away="viewTypeSelectorOpen = false">View
                                             <div class="relative">
-                                                <button type="submit" @click="viewTypeSelectorOpen = !viewTypeSelectorOpen" id="dropdownViewButton" data-dropdown-toggle="dropdown-View" class="flex items-center flowbite-select flowbite_btn bg-gray-50 border border-gray-700 text-gray-900 text-sm ml-2 p-2" name="view" id="" :class="[view != null ? 'active' : '', viewTypeSelectorOpen? 'down' : '']">
-                                                    <span x-text="(view == null || view == 0) ? 'As Reported' : view.replace('(Harmonized)', '')"></span>
+                                                <button type="submit" @click="viewTypeSelectorOpen = !viewTypeSelectorOpen" class="flex items-center flowbite-select flowbite_btn bg-gray-50 border border-gray-700 text-gray-900 text-sm ml-2 p-2" name="view" id="" :class="[view != null ? 'active' : '', viewTypeSelectorOpen? 'down' : '']">
+                                                    <span x-text="(view == null || view == 0) ? 'Standardised Template' : view.replace('(Harmonized)', '')"></span>
                                                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M10.3083 6.19514L7.72167 8.78378L5.135 6.19514C5.01045 6.07021 4.84135 6 4.665 6C4.48865 6 4.31955 6.07021 4.195 6.19514C3.935 6.45534 3.935 6.87566 4.195 7.13585L7.255 10.1982C7.515 10.4584 7.935 10.4584 8.195 10.1982L11.255 7.13585C11.515 6.87566 11.515 6.45534 11.255 6.19514C10.995 5.94161 10.5683 5.93494 10.3083 6.19514Z" fill="#121A0F"/>
                                                     </svg>
@@ -452,7 +452,7 @@
                                                 <div x-cloak x-show="reverseSelectorOpen" class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow  dark:bg-gray-700 dark:divide-gray-600 dropdown-scroll-wrapper">
                                                     <div class="p-3 text-base flex items-center justify-between font-medium">
                                                         <div>Order</div>
-                                                        <svg id="orderClose" width="24" height="24" class="cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <svg @click="reverseSelectorOpen = false" id="orderClose" width="24" height="24" class="cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <path d="M6.4 19L5 17.6L10.6 12L5 6.4L6.4 5L12 10.6L17.6 5L19 6.4L13.4 12L19 17.6L17.6 19L12 13.4L6.4 19Z" fill="#686868"/>
                                                         </svg>
                                                     </div>
