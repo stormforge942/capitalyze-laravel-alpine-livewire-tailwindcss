3bb4e903028f4bc3c7639a9408024121b309f447
ace95322 <ace95322@gmail.com>
2024-01-28 04:31:11 +0545

    add custom option

--- resources/views/livewire/company-profile/overview-graph-filters.blade.php
+++ resources/views/livewire/company-profile/overview-graph-filters.blade.php
@@ -1,13 +1,13 @@
 <form class="px-1 flex flex-col xl:flex-row items-center gap-8" @submit.prevent="">
-    <div class="w-full xl:w-auto grid grid-cols-3 xl:flex items-center gap-5 text-sm text-gray-medium2">
+    <div class="w-full xl:w-auto grid grid-cols-3 xl:flex items-center gap-5 xl:gap-3 2xl:gap-5 text-sm text-gray-medium2">
         <?php
-        $ranges = ['3m' => '3m', '6m' => '6m', 'YTD' => 'YTD', '1yr' => '1yr', '5yr' => '5yr', 'max' => 'MAX'];
+        $ranges = ['3m' => '3m', '6m' => '6m', 'YTD' => 'YTD', '1yr' => '1yr', '5yr' => '5yr', 'max' => 'MAX', 'custom' => 'Custom'];
         ?>
         @foreach ($ranges as $value => $label)
-            <label class="cursor-pointer flex items-center gap-x-1">
+            <label class="{{ $value === 'custom' ? 'cursor-text' : 'cursor-pointer' }} flex items-center gap-x-1">
                 <input type="radio" name="company-overview-chart-period" value="{{ $value }}"
-                    class="custom-radio focus:ring-0 border-gray-medium2" x-model="currentChartPeriod">
-                <span class="peer-checked:text-dark">{{ $label }}</span>
+                    class="custom-radio focus:ring-0 border-gray-medium2" x-model="currentChartPeriod" @if($value === 'custom') disabled @endif>
+                <span :class="currentChartPeriod === '{{ $value }}' ? 'text-dark' : ''">{{ $label }}</span>
             </label>
         @endforeach
     </div>
