d8a71e986796ae55363baa14ee685a2e11c841ff
ace95322 <ace95322@gmail.com>
2024-02-11 12:43:51 +0545

    show filing content from s3 instead of sec website

---
+++ app/Http/Livewire/CompanyLinkS3Content.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\CompanyLinks;
+use WireElements\Pro\Components\Modal\Modal;
+
+class CompanyLinkS3Content extends Modal
+{
+    public string $s3Link;
+    public string $content;
+    public bool $loaded = false;
+
+    public function mount(array $row)
+    {
+        $this->s3Link = CompanyLinks::query()
+            ->where([
+                'symbol' => $row['symbol'],
+                'acceptance_time' => $row['acceptance_time'],
+                'form_type' => $row['form_type'],
+            ])
+            ->first()
+            ?->s3_link;
+    }
+
+    public function render()
+    {
+        return view('livewire.company-link-s3-content');
+    }
+
+    public function loadData()
+    {
+        $this->content = file_get_contents($this->s3Link) ?? '';
+        $this->loaded = true;
+    }
+
+    public static function attributes(): array
+    {
+        return [
+            'size' => '4xl',
+        ];
+    }
+}
