8c6bd368c2e9f939863d63681747e2de7dd5ce58
ace95322 <ace95322@gmail.com>
2024-03-16 23:24:43 +0545

    fix table alignment

--- resources/views/components/review-number-button.blade.php
+++ resources/views/components/review-number-button.blade.php
@@ -3,9 +3,9 @@
     @can('review-data')
         <button
             @click.stop="Livewire.emit('slide-over.open', 'company-segment-report-slide', [Number(amount) || 0, date, location.href])"
-            class="shrink-0 ml-1 bg-white hover:bg-black text-dark hover:text-white rounded-full">
+            class="shrink-0 ml-1 text-dark">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                class="h-3 w-3">
+                class="h-3 w-auto -mr-1">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
             </svg>
