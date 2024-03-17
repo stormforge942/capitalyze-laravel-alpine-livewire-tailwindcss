55d488a5eb01b2fc67d13bde190a8acdf27ea549
ace95322 <ace95322@gmail.com>
2024-03-18 01:53:50 +0545

    ability to update mutual funds using quarter

--- app/Http/Livewire/Ownership/MutualFunds.php
+++ app/Http/Livewire/Ownership/MutualFunds.php
@@ -2,22 +2,29 @@
 
 namespace App\Http\Livewire\Ownership;
 
-use App\Http\Livewire\AsTab;
 use Livewire\Component;
+use App\Http\Livewire\AsTab;
+use Illuminate\Support\Carbon;
+use App\Models\MutualFundsPage;
+use Illuminate\Support\Facades\DB;
 
 class MutualFunds extends Component
 {
     use AsTab;
 
     public string $ticker;
+    public array $quarters;
     public array $filters = [
         'periodRange' => null,
+        'quarter' => null,
         'search' => ''
     ];
 
     public function mount(array $data = [])
     {
         $this->ticker = $data['company']['ticker'];
+
+        $this->quarters = $this->quarters();
     }
 
     public function render()
@@ -28,5 +35,42 @@ class MutualFunds extends Component
     public function updatedFilters()
     {
         $this->emitTo(MutualFundsTable::class, 'filtersChanged', $this->filters);
+    }
+
+    public function quarters()
+    {
+        $quarters = [];
+
+        $periods =  MutualFundsPage::query()
+            ->where([
+                'symbol' => $this->ticker,
+            ])
+            ->select(
+                DB::raw('MIN(period_of_report) as min'),
+                DB::raw('MAX(period_of_report) as max')
+            )
+            ->first();
+
+        if (!$periods) {
+            return $quarters;
+        }
+
+        $min = Carbon::parse($periods->min)->startOfQuarter();
+        $max = Carbon::parse($periods->max)->endOfQuarter();
+
+        // get all the quarters in the format QX-YYYY
+        while ($min->lte($max)) {
+            $key = $min->clone()->startOfQuarter()->format('Y-m-d') . '|' . $min->clone()->endOfQuarter()->format('Y-m-d');
+            $value = 'Q' . $min->quarter . '-' . $min->year;
+            $quarters[$key] = $value;
+
+            $min->addQuarter();
+        }
+
+        $quarters = array_reverse($quarters);
+
+        $this->filters['quarter'] = array_key_first($quarters);
+
+        return $quarters;
     }
 }
