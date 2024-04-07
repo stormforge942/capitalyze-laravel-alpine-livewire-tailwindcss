4f12dfe19d7bcba373627c37515a20ae3f5d1605
ace95322 <ace95322@gmail.com>
2024-04-07 19:13:24 +0545

    ability to export table as csv file

--- resources/js/export-table-to-excel.js
+++ resources/js/export-table-to-excel.js
@@ -4,6 +4,8 @@ function initTableExport(container) {
         (table) => table.parentElement.tagName !== "TD"
     )
 
+    let btns = []
+
     tables.forEach((table, idx) => {
         const wrapper = document.createElement("div")
 
@@ -14,8 +16,7 @@ function initTableExport(container) {
 
         const button = document.createElement("button")
         button.innerHTML = `Download <svg class="h-4 w-4" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3"></path>
-      </svg>`
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3"></path></svg>`
 
         button.style.display = "flex"
         button.style.alignItems = "center"
@@ -30,12 +31,55 @@ function initTableExport(container) {
 
         button.addEventListener("click", () => {
             exportTableToExcel(table, idx + 1)
+
+            // not sure why but buttons are disabled after click
+            setTimeout(() => {
+                btns.forEach((btn) => (btn.disabled = false))
+            }, 1000)
         })
 
         wrapper.appendChild(button)
 
         table.before(wrapper)
+
+        btns.push(button)
     })
+
+    // download all tables btn
+    if (tables.length) {
+        const button = document.createElement("button")
+        button.innerHTML = `Download All Tables<svg class="h-4 w-4" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3"></path></svg>`
+        button.style.display = "flex"
+        button.style.alignItems = "center"
+        button.style.fontSize = "12px"
+        button.style.columnGap = "8px"
+        button.style.fontWeight = "500"
+        button.style.backgroundColor = "white"
+        button.style.border = "1px solid #e5e7eb"
+        button.style.padding = "0.25rem 0.5rem"
+        button.style.borderRadius = "0.5rem"
+        button.style.cursor = "pointer"
+
+        button.addEventListener("click", () => {
+            tables.forEach((table, idx) => {
+                exportTableToExcel(table, idx + 1)
+            })
+
+            // not sure why but buttons are disabled after click
+            setTimeout(() => {
+                btns.forEach((btn) => (btn.disabled = false))
+            }, 1000)
+        })
+
+        const wrapper = document.createElement("div")
+        wrapper.style.paddingBottom = "0.25rem"        
+
+        wrapper.appendChild(button)
+
+        container.before(wrapper)
+        btns.push(button)
+    }
 }
 
 window.initTableExport = initTableExport
@@ -96,7 +140,7 @@ function exportTableToExcel(table, idx) {
 
         // if first column is empty for row, shift it to left
         data.forEach((row) => {
-            if (!row[0]) {
+            if (!row[0] && !row[1]) {
                 row.shift()
                 row.push("")
             }
@@ -108,6 +152,13 @@ function exportTableToExcel(table, idx) {
         }
         if (data[data.length - 1].every((cell) => cell === "")) {
             data.pop()
+        }
+
+        // remove empty columns or contains only currency symbols
+        for (let i = 0; i < maxColumns; i++) {
+            if (data.every((row) => !row[i])) {
+                data.forEach((row) => row.splice(i, 1))
+            }
         }
     } else {
         data.push(["No data found"])
