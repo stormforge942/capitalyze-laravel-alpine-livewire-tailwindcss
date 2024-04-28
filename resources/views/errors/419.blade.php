372fbd6965f0674156142c36f92e96781c485a5c
ace95322 <ace95322@gmail.com>
2024-04-29 00:16:31 +0545

    proper error message

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -3,3 +3,6 @@
 @section('title', __('Page Expired'))
 @section('code', '419')
 @section('message', __('Page Expired'))
+@section('description')
+It seems you had left this tab inactive for some time, please go <a href="{{ url()->previous()  }}" style="color: un; text-decoration: underline">back</a> and try again.
+@endsection
