839de0b839bf0c3e77660dd5468505be1a566089
ace95322 <ace95322@gmail.com>
2024-08-05 20:12:46 +0200

    fix company multiple ticker for one cik

--- app/Console/Commands/CreateCompanies.php
+++ app/Console/Commands/CreateCompanies.php
@@ -42,18 +42,28 @@ class CreateCompanies extends Command
                 if (isset($value["ticker"]) && !empty($value["ticker"])) {
                     Log::debug("Ticker is set and not empty: {$value["ticker"]}");
                     try {
-                        $company = Company::updateOrCreate(
-                            ['cik' => $value["cik_str"]],
-                            ['ticker' => $value["ticker"], 'name' => $value["title"]]
-                        );
+                         // A cik can have multiple ticker names
+                        $existingCompany = Company::where('cik', $value["cik_str"])
+                            ->where('ticker', $value["ticker"])
+                            ->first();
+
+                        if (!$existingCompany) {
+                            Company::create([
+                                'cik' => $value["cik_str"],
+                                'name' => $value["title"],
+                                'ticker' => $value["ticker"]
+                            ]);
+                        } else {
+                            Log::info("Company with CIK {$value["cik_str"]} and name {$value["title"]} already exists.");
+                        }
+
                     } catch (\Exception $e) {
-                        Log::error("Error creating or finding company: {$e->getMessage()}");
+                        Log::error("Error creating company: {$e->getMessage()}");
                     }
                 } else {
                     Log::warning("Skipping item $key because ticker is not set or empty");
                 }
-            }
-
+            }         
             $this->info('Companies imported successfully!');
         } else {
             Log::error("Failed to fetch data from SEC: " . $response->body());
